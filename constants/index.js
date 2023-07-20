04f4efabb74e16bacf1c0d75e7bc4dc7715295d4
allirezaabedini <93127600+allirezaabedini@users.noreply.github.com>
2023-07-20 14:26:40 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xdf97b480e1438149b103205caa87cbea8f7c094a]";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
